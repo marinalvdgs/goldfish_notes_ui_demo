668f8b34155d006da2019448defc67e9e0923027
marinalvdgs <marinalvdgs@gmail.com>
2021-03-22 20:20:29 +0600

    add animated fish

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -2,12 +2,42 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+const String fish = 'assets/goldfish.png';
+const Duration splashDuration = Duration(seconds: 3);
+
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController fishController;
+  Animation<Offset> bigFishAnimation;
+  Animation<Offset> littleFishAnimation;
+  Animation<double> opacityAnimation;
+
+  @override
+  initState() {
+    fishController = AnimationController(duration: splashDuration, vsync: this)
+      ..repeat();
+    bigFishAnimation =
+        Tween<Offset>(begin: Offset(0.0, -1.0), end: Offset(0.0, 1.0)).animate(
+            CurvedAnimation(parent: fishController, curve: Curves.slowMiddle));
+    littleFishAnimation = Tween<Offset>(
+            begin: Offset(-0.25, -1.0), end: Offset(-0.25, 1.0))
+        .animate(CurvedAnimation(parent: fishController, curve: Curves.easeIn));
+    opacityAnimation = Tween<double>(begin: 0.8, end: 0.0).animate(
+        CurvedAnimation(parent: fishController, curve: Curves.easeOut));
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    fishController.dispose();
+    super.dispose();
+  }
+
   Widget buildLogo() {
     return Padding(
       padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 7),
@@ -42,6 +72,35 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 
+  Widget buildBigFish() {
+    return FittedBox(
+      fit: BoxFit.none,
+      child: Transform.rotate(
+        angle: pi / 8,
+        child: Image.asset(
+          fish,
+          height: MediaQuery.of(context).size.height / 3,
+        ),
+      ),
+    );
+  }
+
+  Widget buildLittleFish() {
+    return FadeTransition(
+      opacity: opacityAnimation,
+      child: FittedBox(
+        fit: BoxFit.none,
+        child: Transform.rotate(
+          angle: pi / 8,
+          child: Image.asset(
+            fish,
+            height: MediaQuery.of(context).size.height / 5,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -49,7 +108,7 @@ class _SplashScreenState extends State<SplashScreen> {
           gradient: LinearGradient(
             colors: [
               Color(0xFF42C694),
-              Color(0xFF119A52),
+              //Color(0xFF119A52),
               Color(0xFF0B6243),
               Color(0xFF021817),
             ],
@@ -58,6 +117,18 @@ class _SplashScreenState extends State<SplashScreen> {
         ),
         child: Stack(
           children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: SlideTransition(
+                  position: littleFishAnimation, child: buildLittleFish()),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: SlideTransition(
+                  position: bigFishAnimation, child: buildBigFish()),
+            ),
             Align(
               alignment: Alignment.topCenter,
               child: buildLogo(),
