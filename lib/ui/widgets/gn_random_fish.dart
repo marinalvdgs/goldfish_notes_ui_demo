2325bc77970853aa85bfc6e599796db5dfe0b8f7
marinalvdgs <marinalvdgs@gmail.com>
2021-03-26 16:00:54 +0600

    change fish animation

--- lib/ui/widgets/gn_random_fish.dart
+++ lib/ui/widgets/gn_random_fish.dart
@@ -4,41 +4,21 @@ import 'package:flutter/material.dart';
 
 import '../splash_screen.dart';
 
-double getStartAngle(
-    double min, double max, double dx1, double dx2, double dy1, double dy2) {
-  if (dx1 == min && dx2 == max) {
-    if (dy1 <= 0)
-      return 3 * pi / 2 + getSweepAngle(dy2, false);
-    else
-      return pi + getSweepAngle(dy2, true);
-  }
-  if (dx1 == max && dx2 == min) {
-    if (dy1 <= 0)
-      return 0 + getSweepAngle(dy2, true);
-    else
-      return pi / 2 + getSweepAngle(dy2, false);
-  }
-  if (dy1 == min && dy2 == max) {
-    if (dx1 <= 0)
-      return 3 * pi / 2 + getSweepAngle(dx2, true);
-    else
-      return 0 + getSweepAngle(dx2, false);
-  }
-  if (dy1 == max && dy2 == min) {
-    if (dx1 <= 0)
-      return pi + getSweepAngle(dx2, false);
-    else
-      return pi / 2 + getSweepAngle(dx2, true);
-  }
-}
-
-double getSweepAngle(double shift, bool isNeedToRecalculate) {
-  if (shift.floorToDouble().abs() > 1) {
-    isNeedToRecalculate = false;
+double getSweepAngle(double shift) {
+  bool isNeedToRecalculate = false;
+  double shiftAngle = shift < 0.6
+      ? pi / 8
+      : shift < 0.9
+          ? -pi / 4
+          : -pi / 2;
+  if (roundDouble(shift, 1).abs() >= 1) {
+    isNeedToRecalculate = true;
     shift = 1 - shift.abs();
   }
-  if (isNeedToRecalculate) return pi / 2 * (1 + shift);
-  return pi / 2 * shift.abs();
+  if (isNeedToRecalculate) {
+    return pi / 2 * (1 - shift) - pi / 8;
+  }
+  return pi / 2 * shift.abs() + shiftAngle;
 }
 
 double roundDouble(double val, int places) {
@@ -71,41 +51,22 @@ class _GNRandomFishState extends State<GNRandomFish>
   void initState() {
     Random random = widget.random;
     fishController =
-        AnimationController(duration: Duration(seconds: 3), vsync: this)
-          ..repeat();
+        AnimationController(duration: Duration(seconds: 8), vsync: this)
+          ..repeat(period: Duration(seconds: 5));
     randomNum = random.nextDouble();
     opacity = randomNum < 0.1 ? 1.0 : randomNum;
     height = random.nextDouble() * 100 + 25;
     final double x = roundDouble(widget.radius / widget.size.width, 1);
     final double y = roundDouble(widget.radius / widget.size.height, 1);
 
-    final candidates = [
-      Tween(
-        begin: Offset(x + random.nextDouble(), -y),
-        end: Offset(-x - random.nextDouble(), y),
-      ),
-      Tween(
-        begin: Offset(x + random.nextDouble(), y),
-        end: Offset(-x - random.nextDouble(), -y),
-      ),
-      Tween(
-        begin: Offset(x, random.nextDouble() + y),
-        end: Offset(-x, -random.nextDouble() - y),
-      ),
-      Tween(
-        begin: Offset(-x, random.nextDouble() + y),
-        end: Offset(x, -random.nextDouble() - y),
-      )
-    ];
-
-    var tween = candidates[random.nextInt(10000) % 4];
+    var tween = Tween(
+      begin: Offset(x + random.nextDouble(), -y),
+      end: Offset(-x - random.nextDouble(), y),
+    );
     offsetAnimation = tween
         .animate(CurvedAnimation(parent: fishController, curve: Curves.linear));
 
-    double a = candidates.indexOf(tween) < 2 ? y : x;
-    angle = getStartAngle(
-            -a, a, tween.begin.dx, tween.end.dx, tween.begin.dy, tween.end.dy) +
-        pi / 8;
+    angle = 0 + getSweepAngle(tween.end.dx);
     super.initState();
   }
 
